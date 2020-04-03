318bebba1ecaf3eed721d76830ba438db2588180
David Hodgson <davidhodgson@hotmail.co.uk>
2020-04-03 10:18:04 +0100

    Converted likelihood to class

--- R/src/RcppModule.cpp
+++ R/src/RcppModule.cpp
@@ -1,24 +1,12 @@
-//
-//
-//  Created by David Hodgson on 03/02/2020.
-//  Copyright © 2020 David Hodgson. All rights reserved.
-//
-
 #include <Rcpp.h>
+using namespace Rcpp;
 #include "EvaluateLogLikelihood.h"
 
 RCPP_MODULE(EvaluateLogLikelihoodModule) {
-    using namespace Rcpp;
-    
     class_<EvaluateLogLikelihood>( "EvaluateLogLikelihood" )
     .constructor<double, double, NumericVector>()
     .field( "contactMatrix", &EvaluateLogLikelihood::contactMatrix )
     .field( "observedData", &EvaluateLogLikelihood::observedData )
-    //.method( "evaluateLogLikelihoodCpp", &EvaluateLogLikelihood::evaluateLogLikelihoodCpp )
+    .method( "evaluateLogLikelihoodCpp", &EvaluateLogLikelihood::evaluateLogLikelihoodCpp )
     ;
-    /*.constructor<double,double, NumericVector>()
-    .field( "contactMatrix", &EvaluateLogLikelihood::contactMatrix )
-    .field( "observedData", &EvaluateLogLikelihood::observedData )
-    .method( "evaluateLogLikelihood", &EvaluateLogLikelihood::evaluateLogLikelihoodCpp )
-    ;*/
 }
